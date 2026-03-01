0f756deb2659c782c8bf84ddcee978bac41c156f
Halima <halimaouaksim@gmail.com>
2026-03-01 00:32:28 +0000

    l'injection des dépendances avec Spring Version XML

--- src/main/java/net/ouaksim/metier/MetierImpl.java
+++ src/main/java/net/ouaksim/metier/MetierImpl.java
@@ -1,5 +1,6 @@
 package net.ouaksim.metier;
 
+import net.ouaksim.dao.DaoImpl;
 import net.ouaksim.dao.IDao;
 
 public class MetierImpl implements IMetier{
@@ -9,9 +10,16 @@ public class MetierImpl implements IMetier{
         this.dao = dao;
     }
 
+    public MetierImpl() {
+    }
+
     @Override
     public double calcul() {
         double data = dao.getData();
         return data * 43/3;
+    }
+
+    public void setDao(DaoImpl dao) {
+        this.dao = dao;
     }
 }
---
+++ src/main/java/net/ouaksim/presentation/PresSpringXml.java
@@ -0,0 +1,15 @@
+package net.ouaksim.presentation;
+
+import net.ouaksim.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresSpringXml {
+    public static void main(String[] args) {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+
+        IMetier metier = (IMetier) springContext.getBean("metier");
+        System.out.println(metier.calcul());
+
+    }
+}
